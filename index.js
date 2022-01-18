41b90e234efcc360df453d2464c4eb197f846662
unknown <janeaustine0@gmail.com>
2022-01-18 23:30:16 +0600

    Complete Shopping-cart

--- index.js
+++ index.js
@@ -1,24 +1,76 @@
 ////// Case
 
-function handleProductChange(isIncrease) {
-  const caseInput = document.getElementById("case-count");
-  const caseCount = parseInt(caseInput.value);
-    let caseNewCount = caseCount;
+function handleProductChange(product,isIncrease) {
+      // ######## this #######
+  // const productInput = document.getElementById(product + "-count");
+  // const productCount = parseInt(productInput.value);
+  const productCount = getInputValue(product);
+    let productNewCount = productCount;
     if (isIncrease == true) {
-      caseNewCount = caseCount + 1;
+      productNewCount = productCount + 1;
     }
-    if (isIncrease == false && caseCount > 0) {
-      caseNewCount = caseCount - 1;
+    if (isIncrease == false && productCount > 0) {
+      productNewCount = productCount - 1;
     }
-    caseInput.value = caseNewCount;
-    const caseTotal = caseNewCount * 59;
-    document.getElementById("case-total").innerText = "$" + caseTotal;
+    // ######### or ######## 
+    document.getElementById(product + "-count").value = productNewCount;
+    let productTotal = 0;
+    if(product == 'phone'){
+    productTotal = productNewCount * 1219;
+    }
+    if(product == 'case'){
+    productTotal = productNewCount * 59;
+    }
+    document.getElementById(product + "-total").innerText = "$" + productTotal;
+    calculateTotal();
+}
+
+
+function calculateTotal(){
   
+  const phoneCount = getInputValue("phone");
+  const caseCount = getInputValue("case");
+  
+  const totalPrice = phoneCount * 1219 + caseCount * 59;
+  document.getElementById("total-price").innerText = "$" + totalPrice;
+  const tax = Math.round(totalPrice * 0.1); // 10%
+  document.getElementById('tax-amount').innerText = '$' + tax;
+  const grandTotal = totalPrice + tax;
+  document.getElementById('grand-total').innerText = '$' + grandTotal;
+}
+
+function getInputValue(product){
+  const productInput = document.getElementById(product + "-count");
+  const productCount = parseInt(productInput.value);
+  return productCount;
 }
 
 
 
-// ####################### Add Items ##################### 
+
+
+
+
+
+
+
+
+// ##################### OR ######################### 
+
+// function handlePhoneChange(isIncrease){
+//   const phoneInput = document.getElementById("phone-count");
+//   const phoneCount = parseInt(phoneInput.value);
+//   phoneNewCount = phoneCount;
+//   if(isIncrease == true){
+//   phoneNewCount = phoneCount + 1;
+//   }
+//   if(isIncrease == false && phoneCount > 0){
+//    phoneNewCount = phoneCount - 1;
+//   }
+//   phoneInput.value = phoneNewCount;
+//   const phoneTotal = phoneNewCount * 1219;
+//   document.getElementById("phone-total").innerText = "$" + phoneTotal;
+// }
 
 // document.getElementById("case-increase").addEventListener("click", function () {
 //   const caseInput = document.getElementById("case-count");
@@ -29,3 +81,12 @@ function handleProductChange(isIncrease) {
 //   document.getElementById("case-total").innerText = "$" + caseTotal;
 // });
 
+//function calculateTotal(){
+//   const phoneInput = document.getElementById('phone-count');
+//   const phoneCount = parseInt(phoneInput.value);
+
+//   const caseInput = document.getElementById('case-count');
+//   const caseCount = parseInt(caseInput.value);
+//   const totalPrice = phoneCount * 1219 + caseCount * 59;
+//   document.getElementById("total-price").innerText = "$" + totalPrice;
+// }
